c28fd5e1c609426d8a16d9412bdd9832f26c0b5c
Vitor Dalbem Costa <dalbemcosta@gmail.com>
2022-01-21 09:38:22 +0000

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,7 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 
-const movies = require('./data');
+const movies = require('./data.js');
 
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
@@ -26,43 +26,19 @@ function howManyMovies(onlyDirectors) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 
-function scoresAverage(movies){
-  const avgScores = movies.reduce(function (accumulator, currentValue ) {
-    return accumulator + movies.score
-  }, 0)
-  const avegScores = sumScores / movies.length... 
-
-}
-
+function scoresAverage() {}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
-function dramaMoviesScore(movies) {
-  const DramaFilms = movies.filter(function(movie){
-    return (movie.genre.includes("Drama"))
-  })
-  const sumScores = DramaFilms.reduce(function(global, scores){
-    return global + scores.score
-  }, 0)
-  const averageScores = sumScores / DramaFilms.length
-  const averageScoresDecimals = Math.round(averageScores * 100)/100
-  return averageScoresDecimals
-}
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(movies){
-  const sorted =JSON.parse(JSON.stringify(movies)).sort(function(a, b){
-    if(a.year !== b.year){
-      return a.year - b.year;
-    }else{
-      return a.title.localeCompare(b.title);
-    }
-  })
-  return sorted
-}
+function orderByYear() {}
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
